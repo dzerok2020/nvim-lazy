12f5ca2786b173bf493fc1dc9a93641fbcd2bec2
Duy Khanh <dzerok2020@gmail.com>
2023-03-06 22:18:17 +0700

    feat(copilot): better sorting for cmp sources when using copilot

--- lua/nvim-lazy/plugins/extras/coding/copilot.lua
+++ lua/nvim-lazy/plugins/extras/coding/copilot.lua
@@ -35,6 +35,7 @@ return {
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require("cmp")
+      -- opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "copilot" } }))
       opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "copilot" } }))
 
       local confirm = opts.mapping["<CR>"]
@@ -52,6 +53,25 @@ return {
           return confirm(...)
         end,
       })
+
+      opts.sorting = {
+        priority_weight = 2,
+        comparators = {
+          require("copilot_cmp.comparators").prioritize,
+
+          -- Below is the default comparitor list and order for nvim-cmp
+          cmp.config.compare.offset,
+          -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+          cmp.config.compare.recently_used,
+          cmp.config.compare.locality,
+          cmp.config.compare.kind,
+          cmp.config.compare.sort_text,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
+        },
+      }
     end,
   },
 }
