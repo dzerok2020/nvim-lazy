e52fef726c10e371c2cdcd7d38fe64bbc3e248de
Duy Khanh <dzerok2020@gmail.com>
2023-03-14 23:04:16 +0700

    fix(core): set cond=true for LazyVim

--- lua/nvim-lazy/plugins/core.lua
+++ lua/nvim-lazy/plugins/core.lua
@@ -1,6 +1,6 @@
 require("nvim-lazy.config").init()
 
 return {
-  { "folke/lazy.nvim", version = "*" },
-  { "dzerok2020/nvim-lazy", priority = 10000, lazy = false, config = true, version = "*" },
+	{ "folke/lazy.nvim", version = "*" },
+	{ "dzerok2020/nvim-lazy", priority = 10000, lazy = false, config = true, cond = true, version = "*" },
 }
