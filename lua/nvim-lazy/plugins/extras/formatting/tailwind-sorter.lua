7436fe74e4245deb30f441ef707898ee7de309f5
Duy Khanh <dzerok2020@gmail.com>
2023-03-16 11:16:25 +0700

    fix tailwind-sorter: keys and on_save_enabled = false

--- lua/nvim-lazy/plugins/extras/formatting/tailwind-sorter.lua
+++ lua/nvim-lazy/plugins/extras/formatting/tailwind-sorter.lua
@@ -3,11 +3,10 @@ return {
 		"laytan/tailwind-sorter.nvim",
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
 		build = "cd formatter && npm i && npm run build",
-		config = {
-			on_save_enabled = true,
-		},
+		config = {},
 		keys = {
-			{ "<leader>cs", "<cmd>TailwindSort<cr>", desc = "sorts classes in the current buffer" },
+			{ "<leader>cs", "<cmd>TailwindSort<cr>", desc = "Sorts classes in the current buffer" },
+			{ "<leader>ct", "<cmd>TailwindSortOnSaveToggle<cr>", desc = "Toggles automatic sorting on save" },
 		},
 	},
 }
