d6ff577f9fa80688895e7cd0d4d66cdb1cc77aa1
Duy Khanh <dzerok2020@gmail.com>
2023-03-16 11:10:17 +0700

    added tailwind sorter

---
+++ lua/nvim-lazy/plugins/extras/formatting/tailwin-sorter.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"laytan/tailwind-sorter.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
+		build = "cd formatter && npm i && npm run build",
+		config = {
+			on_save_enabled = true,
+		},
+		keys = {
+			{ "<leader>cs", "<cmd>TailwindSort<cr>", desc = "sorts classes in the current buffer" },
+		},
+	},
+}
